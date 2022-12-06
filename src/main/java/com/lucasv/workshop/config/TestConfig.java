03fc16f2cd286ab8fcdc8cf10ab541a89dc4b9f4
Power Sistemas <lukasfv10@gmail.com>
2022-12-06 16:03:51 -0300

    OrderItem, many-to-many continuacao

--- src/main/java/com/lucasv/workshop/config/TestConfig.java
+++ src/main/java/com/lucasv/workshop/config/TestConfig.java
@@ -1,14 +1,8 @@
 package com.lucasv.workshop.config;
 
-import com.lucasv.workshop.entities.Category;
-import com.lucasv.workshop.entities.Order;
-import com.lucasv.workshop.entities.Product;
-import com.lucasv.workshop.entities.User;
+import com.lucasv.workshop.entities.*;
 import com.lucasv.workshop.entities.enums.OrderStatus;
-import com.lucasv.workshop.repositories.CategoryRepository;
-import com.lucasv.workshop.repositories.OrderRepository;
-import com.lucasv.workshop.repositories.ProductRepository;
-import com.lucasv.workshop.repositories.UserRepository;
+import com.lucasv.workshop.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -32,6 +26,10 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
@@ -65,5 +63,12 @@ public class TestConfig implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
     }
 }
